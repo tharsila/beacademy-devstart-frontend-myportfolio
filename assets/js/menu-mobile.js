5b9af1511bde6b47124c2ac5324a8ab71a232237
tharsila <tharsilapaula@gmail.com>
2022-05-18 02:13:53 -0300

    add menu mobile

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,21 @@
+export default function menuMobile() {
+  const menuHamburger = document.getElementById('menu-mobile');
+  const menuList = document.getElementById('menu-list');
+  const events = ['click', 'touchstart']
+  
+  function handleMenu (event) {
+    if (event.type === 'touchstart') event.preventDefault();
+  
+    menuList.classList.toggle('active');
+    menuHamburger.classList.toggle('fa-times');
+    
+    window.addEventListener('scroll', () => {
+      menuList.classList.remove('active');
+    menuHamburger.classList.remove('fa-times');
+    })
+  }
+
+  events.forEach((event) => {
+    menuHamburger.addEventListener(event, handleMenu);
+  })
+}
